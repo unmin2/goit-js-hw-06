77cb153adf98f056b2d6ffb7784bc660e64d4e9b
unmin2 <125081601+unmin2@users.noreply.github.com>
2023-06-02 14:05:30 +0300

    f

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -6,8 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const list = document.querySelector('.ingredients');
-const firstItem = document.createElement('li');
-firstItem.textContent = 'Potatoes';
-const list.prepend(firstItem);
+const listEl = document.querySelector('.ingredients');
+let litags = '';
+ingredients.forEach(ingredients => {
+  const listEl = `<li> class="item"</li>`;
+  listEl += litags;
 console.log(ingredients);
+})
+
+
+// let liTags = '';
+// images.forEach(image => {
+//   const liEl = `<li class="gallery-item"><img scr=${image.url} alt="${image.alt}" width="480" height="240"/></li>`; 
+//   liTags += liEl;
+// });
+// listEl.insertAdjacentHTML('afterbegin', liTags);
---
+++ src/src/js/task-05.js
@@ -0,0 +1,5 @@
+const input = document.querySelector(`#name-input`);
+const output = document.querySelector(`#name-output`);
+input.addEventListener("input", (event) => {
+    output.textContent = event.currentTarget.value === "" ? "Anonymous" : event.currentTarget.value;
+})
