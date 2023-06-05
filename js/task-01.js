65343cedf802c098e32bdf724ab250a15279e46f
unmin2 <125081601+unmin2@users.noreply.github.com>
2023-06-05 10:26:59 +0300

    s

--- js/task-01.js
+++ js/task-01.js
@@ -1,37 +1,10 @@
-const categoriesEl = document.querySelectorAll('#categories .item');
-let categoriesQuantity = 0;
-categoriesEl.forEach(() => {
-    categoriesQuantity += 1;
-})
-console.log(`Number of categories: ${categoriesQuantity}`);
 
-const firstCategory = categoriesEl[0];
-const firstHeader = firstCategory.querySelector('h2');
-console.log(`Category: ${firstHeader.textContent}`);
-const firstElements = firstCategory.querySelectorAll('ul li');
-let firstElementsQuantity = 0;
-firstElements.forEach(() => {
-    firstElementsQuantity += 1;
+
+const categoriesEl = document.getElementById('categories').children.length;
+console.log(`Number of categories: ${categoriesEl}`);
+
+const arrayTitlesCategoriesRef = document.querySelectorAll('h2');
+arrayTitlesCategoriesRef.forEach((title) => {
+  console.log('Category:', title.textContent);
+  console.log('Elements:', title.nextElementSibling.children.length);
 });
-console.log(`Elements: ${firstElementsQuantity}`);
-
-
-const secondCategory = categoriesEl[1];
-const secondHeader = secondCategory.querySelector('h2');
-console.log(`Category: ${secondHeader.textContent}`);
-const secondElements = secondCategory.querySelectorAll('ul li');
-let secondElementsQuantity = 0;
-secondElements.forEach(() => {
-    secondElementsQuantity += 1;
-});
-console.log(`Elements: ${secondElementsQuantity}`);
-
-const thridCategory = categoriesEl[2]
-const thridHeader = thridCategory.querySelector('h2');
-console.log(`Category: ${thridHeader.textContent}`);
-const thridElements = thridCategory.querySelectorAll('ul li');
-let thridElementsQuantity = 0;
-thridElements.forEach(() => {
-    thridElementsQuantity += 1;
-});
-console.log(`Elements: ${thridElementsQuantity}`);
--- js/task-02.js
+++ js/task-02.js
@@ -7,29 +7,11 @@ const ingredients = [
   'Condiments',
 ];
 const ingredientsEl = document.querySelector('#ingredients');
-const firstLiEl = document.createElement('li');
-firstLiEl.classList.add('item');
-firstLiEl.textContent = 'Potatoes'
+const itemsEl = ingredients.map((ingredient) => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  return item;
+});
 
-
-const secondLiEl = document.createElement('li');
-secondLiEl.classList.add('item');
-secondLiEl.textContent = 'Mushrooms'
-
-
-const thridLiEl = document.createElement('li');
-thridLiEl.classList.add('item');
-thridLiEl.textContent = 'Mushrooms'
-
-const fouthLiEl = document.createElement('li');
-fouthLiEl.classList.add('item');
-fouthLiEl.textContent = 'Tomatos'
-
-
-const fifthLiEl = document.createElement('li');
-fifthLiEl.classList.add('item');
-fifthLiEl.textContent = 'Herbs'
-
-ingredientsEl.append(firstLiEl, secondLiEl, thridLiEl, fouthLiEl, fifthLiEl);
-
-console.log(ingredientsEl);
+ingredientsEl.append(...itemsEl);
